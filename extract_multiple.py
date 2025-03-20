b92b5d04b3bcf921ad6766f3dd3cc3906c77825f
HiteshAroraCool <76745725+HiteshAroraCool@users.noreply.github.com>
2025-03-20 17:32:53 +0530

    scan folder for pdf

--- extract_multiple.py
+++ extract_multiple.py
@@ -1,10 +1,12 @@
 import os
 from extract_data import *
 from write_to_csv import *
-folder = "F:\work\Miscellaneous\Food Order Analyst\Invoices\Zomato"
 
-for filename in os.listdir(folder):
-    if filename.endswith(".pdf"):
-        pdf_path = os.path.join(folder, filename)
-        invoice_dict = convert_to_dict(pdf_path)
-        write_invoice_data_to_csv(invoice_dict)
+# Provide Invoices file path
+folder = "F:\work\Miscellaneous\Food Order Analyst\Invoices"
+for root, dirs, files in os.walk(folder):
+    for filename in files:
+        if filename.endswith(".pdf"):
+            pdf_path = os.path.join(root, filename)
+            invoice_dict = convert_to_dict(pdf_path)
+            write_invoice_data_to_csv(invoice_dict)
